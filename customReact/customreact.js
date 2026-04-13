ed84bdfedd78ec036269a10a1a1adb92c5c7de05
ashu <ashutoshvishwakarma222@gmail.com>
2026-04-13 09:15:27 +0530

    making my custom react

---
+++ customReact/customreact.js
@@ -0,0 +1,22 @@
+
+function customRender(reactElement, conatiner){
+    const domElement = document.createElement(reactElement.type);
+    domElement.innerHTML = reactElement.children;
+    domElement.setAttribute('href', reactElement.props.href);
+    domElement.setAttribute('target', reactElement.props.target);
+
+    conatiner.appendChild(domElement);
+}
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com/',
+        target: '_blank'
+    },
+    children: 'Click me to visit Google'
+}
+
+const mainContainer = document.getElementById('root');
+
+customRender(reactElement, mainContainer);
