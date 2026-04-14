2ff61284f046ce6ce44477e853171a7f30f1f650
ashu <ashutoshvishwakarma222@gmail.com>
2026-04-14 10:40:26 +0530

    added basics of props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,31 +1,17 @@
+import Card from "./components/Card";
+
 function App() {
   return (
     <>
       <h1 className="bg-green-400 text-black text-center p-10 mx-80 my-2 text-9xl rounded-3xl">
         Tailwind Test
       </h1>
+      <Card username="Ashutosh" price={2300}/>
+      <Card username="Navin" price={2100}/>
+      <Card price={2000}/>
+      <Card username="Suraj" price={2500}/>
 
-      <div className=" w-72 flex flex-col rounded-xl glass  min-h-72 ml-150 mt-15">
-        <div>
-          <img
-            src="https://media4.giphy.com/media/1gbqIc1fK8QgR3bHL7/giphy.gif?cid=790b7611a2f696d51a46ce892e420e77735707466a4abd3b&rid=giphy.gif&ct=g"
-            alt="test"
-            width="300"
-            height="300"
-            className="rounded-t-xl w-full"
-          />
-        </div>
-        <div className="flex flex-col py-3 px-3 pb-7 -mt-4 bg-orange-700 rounded-b-xl ">
-          <div className="flex justify-between">
-            <h1 className="font-RubikBold ">Bored ape nft accidental</h1>
-            <h1 className="font-bold font-RubikBold">Price</h1>
-          </div>
-          <div className="flex  justify-between font-mono">
-            <p>#345</p>
-            <p>0.01</p>
-          </div>
-        </div>
-      </div>
+     
     </>
   );
 }
