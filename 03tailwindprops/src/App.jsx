7ca6bfb08662480e3cb1a61ef8049a90fbe30faa
ashu <ashutoshvishwakarma222@gmail.com>
2026-04-14 10:08:27 +0530

    added first project with tailwind

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,33 @@
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-400 text-black text-center p-10 mx-80 my-2 text-9xl rounded-3xl">
+        Tailwind Test
+      </h1>
+
+      <div className=" w-72 flex flex-col rounded-xl glass  min-h-72 ml-150 mt-15">
+        <div>
+          <img
+            src="https://media4.giphy.com/media/1gbqIc1fK8QgR3bHL7/giphy.gif?cid=790b7611a2f696d51a46ce892e420e77735707466a4abd3b&rid=giphy.gif&ct=g"
+            alt="test"
+            width="300"
+            height="300"
+            className="rounded-t-xl w-full"
+          />
+        </div>
+        <div className="flex flex-col py-3 px-3 pb-7 -mt-4 bg-orange-700 rounded-b-xl ">
+          <div className="flex justify-between">
+            <h1 className="font-RubikBold ">Bored ape nft accidental</h1>
+            <h1 className="font-bold font-RubikBold">Price</h1>
+          </div>
+          <div className="flex  justify-between font-mono">
+            <p>#345</p>
+            <p>0.01</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
