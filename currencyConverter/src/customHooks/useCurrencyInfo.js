a5d36f1262c53884b7e5dad1aa036b2481fd98cf
ashu <ashutoshvishwakarma222@gmail.com>
2026-04-16 11:00:38 +0530

    fix error of console.log

--- currencyConverter/src/customHooks/useCurrencyInfo.js
+++ currencyConverter/src/customHooks/useCurrencyInfo.js
@@ -8,9 +8,7 @@ function useCurrencyInfo(currency) {
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
-        console.log(data);
     }, [currency])
-    console.log(data);
     return data
 }
 
